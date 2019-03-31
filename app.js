1720786ee232f26962197d10822ba9971fe86ff5
michael murphy <michaelmurphy@salesforce.com>
2019-03-31 18:04:16 +0100

    update

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var express = require('express');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var session = require('express-session');
 var sftools = require('./sf-tools');
 var app = express();
 var PORT = process.env.PORT || 5000;
@@ -6,22 +10,21 @@ var PORT = process.env.PORT || 5000;
 //SF app secret
 var SF_CANVASAPP_CLIENT_SECRET = process.env.SF_CANVASAPP_CLIENT_SECRET;
 
-app.configure(function() {
-    app.use('/public',express.static(__dirname + '/public'));
-    app.engine('html', require('ejs').renderFile);
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'html');
-    app.use(express.cookieParser());
 
-    //session enabled to store token data
-    app.use(express.session({
-      secret: 'cs secret'
-    }));
+app.use('/public',express.static(__dirname + '/public'));
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+app.use(cookieParser());
 
-    app.use(express.bodyParser());
-    app.use(express.logger());
-    
-});
+//session enabled to store token data
+app.use(session({
+    secret: 'cs secret'
+}));
+
+app.use(bodyParser.json({type:'application/*+json'}));
+
+app.use(logger('combined'));
 
 app.get('/',function(req,res){
     //get the canvas details from session (if any)
@@ -46,3 +49,4 @@ app.post('/canvas/callback', function(req,res){
 exports.server = app.listen(PORT, function() {
     console.log("Listening on " + PORT);
 });
+
